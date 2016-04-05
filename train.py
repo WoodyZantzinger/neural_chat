34d884760c2724eae279b9a126e68e070a2f65c3
Woody Zantzinger <woodyzantzinger@WTA-MBP86.local>
2016-04-05 17:27:07 -0400

    .gitignore

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ sentence_end_token = "MESSAGE_END"
 
 
 
-print "Reading JSON message history..."
+print "Reading Slack history"
 with open(sys.argv[1]) as data_file:
     message_data = json.load(data_file)
 
@@ -21,14 +21,17 @@ with open(sys.argv[1]) as data_file:
     # Append SENTENCE_START and SENTENCE_END
     sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
 print "Parsed %d sentences." % (len(sentences))
-     
+
+
 # Tokenize the sentences into words
 tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
- 
+
+
 # Count the word frequencies
 word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
 print "Found %d unique words tokens." % len(word_freq.items())
- 
+
+
 # Get the most common words and build index_to_word and word_to_index vectors
 vocab = word_freq.most_common(vocabulary_size-1)
 index_to_word = [x[0] for x in vocab]
@@ -37,7 +40,8 @@ word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
  
 print "Using vocabulary size %d." % vocabulary_size
 print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
- 
+
+
 # Replace all words not in our vocabulary with the unknown token
 for i, sent in enumerate(tokenized_sentences):
     tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
